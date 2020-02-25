f4a60a70337e360acf58a9f7b3bfbd1006777274
Victor Hugo Vieira <victor.h.souza.vieira@gmail.com>
2020-02-25 19:24:20 -0300

    refactor test bike model

--- spec/models/bike_spec.rb
+++ spec/models/bike_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Bike, type: :model do
-  context 'validations tests' do
-    let(:bike) { create(:bike, code: '9988776655443') }
+  RSpec.describe Bike, type: :model do
+    context 'validations tests' do
+      let(:bike) { create(:bike, code: '9988776655443') }
 
-    it "is valid with valid attributes" do
-      expect(bike).to be_valid
-    end
+      it "is valid with valid attributes" do
+        expect(bike).to be_valid
+      end
 
-    it "is not valid without a code" do
-      expect(Bike.new(code: nil)).to_not be_valid
+      it "is not valid without a code" do
+        bike.code = nil
+
+        expect(bike).to_not be_valid
+      end
     end
   end
-end
