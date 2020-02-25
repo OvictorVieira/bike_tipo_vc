80beeef8dbc1bc90e0ff49bc5479dd4ecabe6127
Victor Hugo Vieira <victor.h.souza.vieira@gmail.com>
2020-02-25 20:40:56 -0300

    creates validation tests for trip

---
+++ spec/factories/trips_factory.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :trip do
+    started_at { Time.now }
+    finished_at { Time.now + 30.minutes }
+    traveled_distance { rand(0.00..99.99) }
+
+    association(:bike)
+    association(:user)
+    association(:station)
+  end
+end
---
+++ spec/models/trip_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe Trip, type: :model do
+  context 'validations tests' do
+    let(:user) { create(:user) }
+    let(:bike) { create(:bike) }
+    let(:station_sao_paulo) { create(:station) }
+    let(:station_da_luz) do
+      create(:station, name: 'Estação da Luz',
+                       latitude: -22.2232155,
+                       longitude: -49.9879748)
+    end
+
+    let(:trip) do
+      create(:trip, user: user,
+                    bike: bike,
+                    origin_station: station_sao_paulo.id,
+                    destination_station: station_da_luz.id)
+    end
+
+    it "is valid with valid attributes" do
+      expect(trip).to be_valid
+    end
+
+    it "is not valid without a user" do
+      trip.user = nil
+
+      expect(trip).to_not be_valid
+    end
+
+    it "is not valid without a bike" do
+      trip.bike = nil
+
+      expect(trip).to_not be_valid
+    end
+
+    it "is not valid without a started_at" do
+      trip.started_at = nil
+
+      expect(trip).to_not be_valid
+    end
+
+    it "is not valid without a finished_at" do
+      trip.finished_at = nil
+
+      expect(trip).to_not be_valid
+    end
+
+    it "is not valid without a origin_station" do
+      trip.origin_station = nil
+
+      expect(trip).to_not be_valid
+    end
+
+    it "is not valid without a destination_station" do
+      trip.destination_station = nil
+
+      expect(trip).to_not be_valid
+    end
+
+    it "is not valid without a traveled_distance" do
+      trip.traveled_distance = nil
+
+      expect(trip).to_not be_valid
+    end
+  end
+end
