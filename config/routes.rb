ca9b7f2069db0b739ac8af5c3d757f72879498ef
Victor Hugo Vieira <victor.h.souza.vieira@gmail.com>
2020-03-03 22:58:29 -0300

    fix login screen that was redirecting when logging in

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    # authenticated :user do
-    #   root 'home#index', as: :authenticated_root
-    # end
+    authenticated :user do
+      root 'trips#index', as: :authenticated_root
+    end
 
     unauthenticated do
       root 'devise/sessions#new', as: :unauthenticated_root
