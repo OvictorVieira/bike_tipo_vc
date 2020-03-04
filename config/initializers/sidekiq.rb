df4347f586d23be970be109b251c1102e66636bd
Victor Vieira <victor.vieira@tray.net.br>
2020-03-04 10:48:45 -0300

    adds validation when logging into sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 url_redis = Rails.application.config.app.redis.host
 
 redis_conn = proc {
@@ -10,4 +12,9 @@ end
 
 Sidekiq.configure_server do |config|
   config.redis = ConnectionPool.new(size: 25, &redis_conn)
+end
+
+Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+  Rack::Utils.secure_compare(::Digest::SHA256.hexdigest(user), ::Digest::SHA256.hexdigest(Rails.application.config.app.sidekiq.user.to_s)) &
+    Rack::Utils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(Rails.application.config.app.sidekiq.password.to_s))
 end
